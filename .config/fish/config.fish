5b69a4f9bf482f7096fd5f6fee14dc28203c2359
Wittano <radoslaw.ratyna@gmail.com>
2022-02-20 11:15:47 +0100

    feat: added new alias
    yta - download mp3 from YouTube

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,7 @@ set -U fish_user_paths $HOME/.local/bin $fish_user_paths
 
 # aliases
 alias pra="sudo pacman -Rsn (pacman -Qdtq)"
+alias yta="youtube-dl -x --audio-format mp3 -o '%(title)s.%(ext)s' --prefer-ffmpeg"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
