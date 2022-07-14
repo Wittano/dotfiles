fec156b4cc0c6a26e4dd6f4b24b8ae86d3bbaa65
Wittano <radoslaw.ratyna@gmail.com>
2022-07-14 21:51:19 +0200

    chore(qtile): removed useless comments

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -16,7 +16,7 @@ import scripts
 from groups import get_default_groups
 from layouts import LayoutsCollection
 from monitors import get_monitors_count, map_wacom_to_one_monitor
-from themes import MyTheme, DraculaTheme
+from themes import DraculaTheme
 from widgets import ScreenCreator
 
 SUPER_KEY = "mod4"
@@ -150,7 +150,6 @@ if WITH_BAR:
 else:
     screens = [screen_creator.create_without_bar() for _ in range(get_monitors_count())]
 
-# Drag floating layouts.
 mouse = [
     Drag([SUPER_KEY], "Button1", lazy.window.set_position_floating(),
          start=lazy.window.get_position()),
@@ -169,33 +168,21 @@ floating_layout = layout.Floating(
     border_normal=theme.border_normal,
     border_width=2,
     float_rules=[
-        # Run the utility of `xprop` to see the wm class and name of an X client.
         *layout.Floating.default_float_rules,
-        Match(wm_class='confirmreset'),  # gitk
-        Match(wm_class='makebranch'),  # gitk
-        Match(wm_class='maketag'),  # gitk
-        Match(wm_class='ssh-askpass'),  # ssh-askpass
-        Match(title='branchdialog'),  # gitk
-        Match(wm_class=re.compile('[pP]inentry*')),  # GPG key password entry
+        Match(wm_class='confirmreset'),
+        Match(wm_class='makebranch'),
+        Match(wm_class='maketag'),
+        Match(wm_class='ssh-askpass'),
+        Match(title='branchdialog'),
+        Match(wm_class=re.compile('[pP]inentry*')),
     ]
 )
 
 auto_fullscreen = True
 focus_on_window_activation = "never"
 reconfigure_screens = True
-
-# If things like steam games want to auto-minimize themselves when losing
-# focus, should we respect this or not?
 auto_minimize = False
 
-# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
-# string besides java UI toolkits; you can see several discussions on the
-# mailing lists, GitHub issues, and other WM documentation that suggest setting
-# this string if your java app doesn't work correctly. We may as well just lie
-# and say that we're a working one by default.
-#
-# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
-# java that happens to be on java's whitelist.
 wmname = "LG3D"
 
 
